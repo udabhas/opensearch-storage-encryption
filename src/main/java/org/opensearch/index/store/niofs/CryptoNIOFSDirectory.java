488e229a3c93e269896398f23b9ed1fb7e8be845
Uday Bhaskar <udabhas@amazon.com>
2025-07-31 08:54:24 +0530

    Added keyFile to not encrypt

--- src/main/java/org/opensearch/index/store/niofs/CryptoNIOFSDirectory.java
+++ src/main/java/org/opensearch/index/store/niofs/CryptoNIOFSDirectory.java
@@ -45,7 +45,7 @@ public class CryptoNIOFSDirectory extends NIOFSDirectory {
 
     @Override
     public IndexInput openInput(String name, IOContext context) throws IOException {
-        if (name.contains("segments_") || name.endsWith(".si") || name.equals("ivFile")) {
+        if (name.contains("segments_") || name.endsWith(".si") || name.equals("ivFile") || name.equals("keyfile")) {
             return super.openInput(name, context);
         }
 
@@ -73,7 +73,7 @@ public class CryptoNIOFSDirectory extends NIOFSDirectory {
 
     @Override
     public IndexOutput createOutput(String name, IOContext context) throws IOException {
-        if (name.contains("segments_") || name.endsWith(".si") || name.equals("ivFile")) {
+        if (name.contains("segments_") || name.endsWith(".si") || name.equals("ivFile") || name.equals("keyfile")) {
             return super.createOutput(name, context);
         }
 
@@ -86,7 +86,7 @@ public class CryptoNIOFSDirectory extends NIOFSDirectory {
 
     @Override
     public IndexOutput createTempOutput(String prefix, String suffix, IOContext context) throws IOException {
-        if (prefix.contains("segments_") || prefix.endsWith(".si") || prefix.equals("ivFile")) {
+        if (prefix.contains("segments_") || prefix.endsWith(".si") || prefix.equals("ivFile") || prefix.equals("keyfile")) {
             return super.createTempOutput(prefix, suffix, context);
         }
 
@@ -100,7 +100,7 @@ public class CryptoNIOFSDirectory extends NIOFSDirectory {
 
     @Override
     public long fileLength(String name) throws IOException {
-        if (name.contains("segments_") || name.endsWith(".si") || name.equals("ivFile")) {
+        if (name.contains("segments_") || name.endsWith(".si") || name.equals("ivFile") || name.equals("keyfile")) {
             return super.fileLength(name);  // Non-encrypted files
         } else {
             Path path = getDirectory().resolve(name);
