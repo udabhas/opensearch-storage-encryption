9801ca88141e1923a9dbe55f654544573b85b2ef
Uday Bhaskar <udabhas@amazon.com>
2026-03-05 08:50:32 +0530

    Revert "reducing time based eviction time for testing"
    This reverts commit ca2f9c454998739f3df3179c6f166869302443ba.

--- src/main/java/org/opensearch/index/store/cache/FileChannelCache.java
+++ src/main/java/org/opensearch/index/store/cache/FileChannelCache.java
@@ -37,8 +37,7 @@ import com.github.benmanes.caffeine.cache.RemovalCause;
 public class FileChannelCache {
     private static final Logger LOGGER = LogManager.getLogger(FileChannelCache.class);
     private static final int MAX_ENTRIES = 100_000;
-    // private static final Duration EXPIRE_AFTER_ACCESS = Duration.ofMinutes(10);
-    private static final Duration EXPIRE_AFTER_ACCESS = Duration.ofSeconds(10);
+    private static final Duration EXPIRE_AFTER_ACCESS = Duration.ofMinutes(10);
 
     /** Primary cache: cacheKey → FileChannel */
     private static final Cache<String, FileChannel> CACHE = Caffeine
